e0967081434f906e385bfd0306de9d1c6600c4b4
USER <USER@LAPTOP-71ATORCR>
2025-11-06 07:12:11 +0530

    push changes

--- GroceryApplication/src/test/java/testscript/NewsTest.java
+++ GroceryApplication/src/test/java/testscript/NewsTest.java
@@ -52,10 +52,48 @@ public class NewsTest extends TestNGBase {
 		WebElement typenews = driver.findElement(By.xpath("//input[@class='form-control']"));
 		typenews.sendKeys("today");
 		WebElement searchnews = driver.findElement(By.xpath("//button[@class='btn btn-danger btn-fix']"));
-		searchnews.click();
-		
-	}
-	
+		searchnews.click();		
+		 WebElement returnhome = driver.findElement(By.xpath("https://groceryapp.uniqassosiates.com/admin/home"));
+		 returnhome.click();
+		}
+	 
+	 @Test
+	 
+	 public void reset() throws IOException {
+		 String usernamevalue = ExcelUtility.getStringData(1, 0, "LoginSheet");//reading data from excel file: data driven approach
+			String passwordvalue = ExcelUtility.getStringData(1, 1, "LoginSheet");
+			//Login to Grocery Application
+			WebElement username = driver.findElement(By.xpath("//input[@name='username']"));
+			username.sendKeys(usernamevalue);
+			WebElement password = driver.findElement(By.xpath("//input[@name='password']"));
+			password.sendKeys(passwordvalue);
+			WebElement signin = driver.findElement(By.xpath("//button[@type='submit']"));
+			signin.click();	
+			//Click Manage News
+			WebElement managenews = driver.findElement(By.xpath("//a[@href='https://groceryapp.uniqassosiates.com/admin/list-news' and @class='small-box-footer']"));
+			managenews.click();
+			//Add news
+			WebElement addnews = driver.findElement(By.xpath("//a[@href='https://groceryapp.uniqassosiates.com/admin/news/add']"));
+			addnews.click();
+			WebElement addnewstext = driver.findElement(By.xpath("//textarea[@id='news']"));
+			addnewstext.sendKeys("Explore news");
+			WebElement savenews = driver.findElement(By.xpath("//button[@type='submit']"));
+			savenews.click();
+			WebElement search = driver.findElement(By.xpath("////a[@class='btn btn-rounded btn-primary']"));
+			search.click();
+			WebElement typenews = driver.findElement(By.xpath("//input[@class='form-control']"));
+			typenews.sendKeys("explore");
+			WebElement searchnews = driver.findElement(By.xpath("//button[@class='btn btn-danger btn-fix']"));
+			searchnews.click();	
+			//reset 
+			WebElement reset = driver.findElement(By.xpath("//i[@class='ace-icon fa fa-sync-alt']"));
+			reset.click();
+			}
+			
+		 
+	 }
+	 
+	 
 	
 
-}
+
