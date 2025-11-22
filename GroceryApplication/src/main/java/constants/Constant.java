b501b6395c29fe670ad269fa10829949c2c41477
JanuSMadhav <janusmadhav@gmail.com>
2025-11-22 13:58:57 +0530

    Delete GroceryApplication directory

--- GroceryApplication/src/main/java/constants/Constant.java
+++
@@ -1,8 +0,0 @@
-package constants;
-
-public class Constant {
-	public static final String TESTDATAFILE = System.getProperty("user.dir")+"\\src\\test\\resources\\TestData.xlsx";//System.getProperty("user.dir") returns the path of Eclipse directory
-	public static final String LOGINSHEET = "LoginSheet";
-	public static final String ADMINTYPE = "Staff";
-	public static final String CONFIGFILE=System.getProperty("user.dir")+"\\src\\main\\resources\\config.properties";
-}
